615ad0e21421dbd3fd5fd6d9b10a0967b1f69a0b
Nadeem Shah <nadeem.abbas.shah90@gmail.com>
2022-09-26 17:05:05 +0500

    circle partial moved to directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Circle } from './components/circle/circle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    Circle,
+    AppComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/circle/circle.component.ts
+++ src/app/components/circle/circle.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 // import employeeData from '../assets/SampleData/employees.json';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: 'circle',
+  templateUrl: './circle.component.html',
+  styleUrls: ['./circle.component.scss'],
 })
-export class AppComponent {
+export class Circle {
   // public employees: Employee[];
   // public relationships: Relationship[];
   public width: number = 10;
@@ -70,47 +70,4 @@ export class AppComponent {
   public increaseRight() {
     this.x = this.x - 50;
   }
-  // public GetEmployee(id: number): any {
-  //   //  Search for, and return, an Employee record, with a particular id value
-  //   const employee = this.employees.filter((emp) => {
-  //     return emp.id === id;
-  //   });
-  //   if (employee == null) {
-  //     return null;
-  //   }
-  //   return employee[0];
-  // }
-
-  // public IsSubManager(relationshipType: string) {
-  //   return relationshipType === 'Sub-manager';
-  // }
-
-  // public mousedown(event: Event) {
-  //   let e = event as MouseEvent;
-
-  //   let element = event.target as HTMLElement;
-  //   let rect = element.getBoundingClientRect();
-
-  //   // element.className
-  //   // alert(`mouse position : ${e.clientX}:${e.clientY}`);
-  //   alert(`element position : ${rect.x}:${rect.y}`);
-  // }
 }
-
-// interface Employee {
-//   id: number;
-//   job: string;
-//   firstName: string;
-//   lastName: string;
-//   imageUrl: string;
-//   DOB: string;
-//   phoneNumber: string;
-//   xpos: number;
-//   ypos: number;
-// }
-
-// interface Relationship {
-//   employeeId: number;
-//   managerId: number;
-//   type: string; // "Manager" or "Sub-manager"
-// }
