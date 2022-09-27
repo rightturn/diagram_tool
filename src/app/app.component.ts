5431ef103fa15541cc0b399265c357cea518ff62
Ejaz Niaz <ejazniaz98@gmail.com>
2022-09-27 15:29:27 +0500

    circle around svg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  public rectangulars: Rectangular[] = [
+    {
+      height: 100,
+      width: 200,
+      color: 'rgba(125, 125, 32, 0.5)',
+      x: 20,
+      y: 30,
+      rx: 0,
+      ry: 0,
+    },
+    {
+      height: 50,
+      width: 50,
+      color: 'rgba(12, 32, 222, 0.7)',
+      x: 200,
+      y: 20,
+      rx: 10,
+      ry: 15,
+    },
+  ];
 
-  public rectangulars: Rectangular[] = [{
-    height: 100,
-    width: 200,
-    color: 'rgba(125, 125, 32, 0.5)',
-    x: 20,
-    y: 30,
-    rx: 0,
-    ry: 0
-  }, {
-    height: 50,
-    width: 50,
-    color: 'rgba(12, 32, 222, 0.7)',
-    x: 200,
-    y: 20,
-    rx: 10,
-    ry: 15
-  }];
-
-
-  constructor() {
-  }
+  // public circleX = 15;
+  // public circleY = 26;
+  public radiusCircle = 10;
+  constructor() {}
 
   /**
-  * Adds new rectangular element.
-  */
+   * Adds new rectangular element.
+   */
   addNew() {
     this.rectangulars.push({
       height: 0,
@@ -41,7 +44,7 @@ export class AppComponent {
       x: 0,
       y: 0,
       rx: 0,
-      ry: 0
+      ry: 0,
     });
   }
 
@@ -79,9 +82,8 @@ export class AppComponent {
   }
 
   public circlePointerdown(event: PointerEvent) {
-    console.log("This is circle");
+    console.log('This is circle');
   }
-
 }
 interface Rectangular {
   height: number;
